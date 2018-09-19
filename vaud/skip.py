27ce033bac3ba87ecd4bb011db4a61fb66a6a3f3
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-09-19 19:36:54 +0200

    implemented build-skip protocol \m/

--- vaud/skip.py
+++ vaud/skip.py
@@ -5,7 +5,8 @@ from twisted.internet import defer
 from twisted.spread import pb
 
 # Define the length of the rs bit string
-RS_LENGTH = 8
+RS_BYTE_LENGTH = 2
+RS_BIT_LENGTH = RS_BYTE_LENGTH * 8
 
 lowest = PseudoNodeReference("lowest")
 highest = PseudoNodeReference("highest")
@@ -43,7 +44,8 @@ pb.setUnjellyableForClass('vaud.core.SkipNodeReference', SkipNodeReference)
 
 def prefix(i: int, v: SkipNodeReference) -> CopyableBitArray:
     """
-    Returns a copy of the first `i` bits of `v`s random bit string (rs) as a bitarray.
+    Returns a copy of the first `i` bits of `v`s random
+    bit string (rs) as a bitarray.
     """
     return v.rs.copy()[:i]
 
@@ -59,56 +61,81 @@ def succ(v: SkipNodeReference, W: Set[SkipNodeReference]) -> SkipNodeReference:
     """
     return min([w for w in W if w > v].append(highest))
 
-def _levelNodes(i: int, v: "SkipNode", x: bool) -> Set(SkipNodeReference):
+def _levelNodes(i: int, v: SkipNodeReference, x: bool, N: Set[SkipNodeReference]) -> Set(SkipNodeReference):
     """
-    Returns {w ∈ N(v) | prefix(i+1, w) = prefix(i, v)◦x}
+    Returns {w ∈ N | prefix(i+1, w) = prefix(i, v)◦x}
     """
     # get prefix(i, v)◦x
-    pref = prefix(i, v.reference)
+    pref = prefix(i, v)
     pref.append(x)
 
     # get {w ∈ N(v) | prefix(i+1, w) = prefix(i, v)◦x}
-    return set(w for w in v.N if prefix(i+1, w) == pref)
+    return set(w for w in N if prefix(i+1, w) == pref)
 
-def levelPred(i: int, v: "SkipNode", x: bool) -> SkipNodeReference:
+def levelPred(i: int, v: SkipNodeReference, x: bool, N: Set[SkipNodeReference]) -> SkipNodeReference:
     """
-    levelPred(i, v, x) = pred(v, {w ∈ N(v) | prefix(i+1, w) = prefix(i, v)◦x})
+    levelPred(i, v, x, N) = pred(v, {w ∈ N | prefix(i+1, w) = prefix(i, v)◦x})
     """
-    return pred(v, _levelNodes(i, v, x))
+    return pred(v, _levelNodes(i, v, x, N))
 
-def levelSucc(i: int, v: "SkipNode", x: bool) -> SkipNodeReference:
+def levelSucc(i: int, v: SkipNodeReference, x: bool, N: Set[SkipNodeReference]) -> SkipNodeReference:
     """
-    levelSucc(i, v, x) = succ(v, {w ∈ N(v) | prefix(i+1, w) = prefix(i, v)◦x})
+    levelSucc(i, v, x, N) = succ(v, {w ∈ N | prefix(i+1, w) = prefix(i, v)◦x})
     """
-    return succ(v, _levelNodes(i, v, x))
+    return succ(v, _levelNodes(i, v, x, N))
 
-def low(i: int, v: "SkipNode") -> SkipNodeReference:
+def low(i: int, v: SkipNodeReference, N: Set[SkipNodeReference]) -> SkipNodeReference:
     """
-    low(i, v) = min{levelPred(i, v, 0), levelPred(i, v, 1)}
+    low(i, v, N) = min{levelPred(i, v, 0, N), levelPred(i, v, 1, N)}
     """
-    return min(levelPred(i, v, 0), levelPred(i, v, 1))
+    return min(levelPred(i, v, 0, N), levelPred(i, v, 1, N))
 
-def high(i: int, v: "SkipNode") -> SkipNodeReference:
+def high(i: int, v: SkipNodeReference, N: Set[SkipNodeReference]) -> SkipNodeReference:
     """
-    high(i, v) = max{levelPred(i, v, 0), levelPred(i, v, 1)}
+    high(i, v, N) = max{levelPred(i, v, 0, N), levelPred(i, v, 1, N)}
     """
-    return max(levelSucc(i, v, 0), levelSucc(i, v, 1))
+    return max(levelSucc(i, v, 0, N), levelSucc(i, v, 1, N))
 
-def range(i: int, v: "SkipNode") -> Set[SkipNodeReference]:
+def skipRange(i: int, v: SkipNodeReference, N: Set[SkipNodeReference]) -> Set[SkipNodeReference]:
     """
-    range(i, v) = [low(i, v), high(i, v)]
+    range(i, v, N) = [low(i, v, N), high(i, v, N)]
     """
-    return set(w for w in v.N if low(i, v) <= w and w <= high(i, v))
+    vPrefix = prefix(i,v)
+    l = low(i, v, N)
+    h = high(i, v, N)
+    return set(w for w in N if prefix(i, w) == vPrefix and l <= w and w <= h)
+
+def commonPrefixLength(v: bitarray, w: bitarray) -> int:
+    """
+    Returns the number of bits of the longest common
+    prefix of v's and w's random bit strings.
+    """
+    i = 0
+    while i < min(len(v), len(w)) and v[i] == w[i]:
+        i += 1
+    return i
+
+def longestCommonPrefixNode(w: SkipNodeReference, W: Set[SkipNodeReference]) -> SkipNodeReference:
+    """
+    Returns the SkipNodeReference of the node in W that has
+    the longest common random bit string prefix with w.
+    """
+    return max(W, key=lambda x: commonPrefixLength(x.rs, w.rs))
 
 class SkipNode(Node):
     
     def __init__(self, port: int):
         super(SkipNode, self).__init__(port)
-        self._rs = CopyableBitArray(randomBitArray(RS_LENGTH)) # random bitstring
+        self._rs = CopyableBitArray(randomBitArray(RS_BYTE_LENGTH)) # random bitstring
         # the self.reference object will serve as the node's id
         # replacing the super constructor's NodeReference by a SkipNodeReference
-        self.reference = SkipNodeReference(self.reference.host, port, self.rs)
+        self.reference = SkipNodeReference(self.reference.host, port, self._rs)
         self.N = set() # outgoing neighborhood
+        self._ranges = {} # range for each level i < RS_BIT_LENGTH - 1
+
+        # a set of all nodes (SkipNodeReferences) that are currently
+        # in at least one of this node's ranges
+        self._nodesInRanges = set()
     
     @remoteMethod(constantReturnValue=True)
     def rs(self):
@@ -118,3 +145,65 @@ class SkipNode(Node):
         want to use its rs attribute, instead of maybe dealing with a deferred.
         """
         return self._rs
+    
+    # "Build-Skip" methods
+    
+    def updateRanges(self):
+        nodesInRanges = set()
+        for i in range(RS_BIT_LENGTH-1):
+            currentLevelRange = skipRange(i, self.reference, self.N)
+            self._ranges[i] = currentLevelRange
+            nodesInRanges.union(currentLevelRange)
+        self._nodesInRanges = nodesInRanges
+    
+    def timeout(self):
+        # See Chapter 5, Slide 169 f.
+        for i in range(RS_BIT_LENGTH-1):
+            # partition range of level i by left and right nodes
+            leftRange = []
+            rightRange = []
+            for x in self._ranges[i]:
+                if x < self.reference:
+                    leftRange.append(x)
+                if x > self.reference:
+                    rightRange.append(x)
+
+            # introduce nodes as shown below
+            # leftRange: v1 -> v2 -> ... -> self
+            # rightRange: self <- ... <- v(n-1) <- vn
+
+            leftRange.sort()
+            rightRange.sort(reverse=True)
+
+            # Part a: Linearizing
+            for r in (leftRange, rightRange):
+                for i in range(len(r)-1):
+                    r[i].linearise(r[i+1])
+                if len(r) > 0:
+                    # introduce closest node to self
+                    r[-1].linearise(self.reference)
+        
+            # Part b: Bridging (as on slide 170)
+            # - left nodes to first right node
+            # - right nodes to first left node
+
+            for range1, range2 in ((leftRange, rightRange), (rightRange, leftRange)):
+                if len(range2) > 0:
+                    closestRange2Node = range2[-1] # last node in right resp. left range
+                    for v in range1:
+                        if closestRange2Node in skipRange(i, v, self.N):
+                            # this node thinks that closestRange2Node is in v's range
+                            v.linearise(closestRange2Node)
+    
+    @remoteMethod
+    def linearise(self, u: SkipNodeReference):
+        # See Chapter 5, Slide 171
+        if u != self.reference and u not in self.N:
+            self.N.add(u)
+            self.updateRanges()
+            undesirableNodes = self.N.difference(self._nodesInRanges) # nodes that are not in any range now
+            self.N = self._nodesInRanges # only keep the relevant nodes in our neighborhood
+            # delegate the undesirable nodes
+            for w in undesirableNodes:
+                delegationDestination = longestCommonPrefixNode(w, self.N)
+                delegationDestination.linearise(w)
